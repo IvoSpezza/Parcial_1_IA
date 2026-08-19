f3df922c28c7858253c9a293cbf7ea4dc106cb07
Arise <83659212+IvoSpezza@users.noreply.github.com>
2026-08-19 11:46:36 -0300

    Modificaciones varias
    Se agregaron todos los comportamientos a este mismo script.
    Se cambio el metodo de seleccion de comportamiento a un enum
    Se agrego el comportamiento Pursuit: (predecir la ubicacion futura del objetivo y perseguirla)
    Se agrego el comportamiento Evade:(predecir la ubicacion futura del objetivo y escaparle)
    Se limpio el codigo borrando secciones de codigo repetido:
    *CalculateDesired
    *CalculateSteering

--- Assets/Scripts/MovementScripts/Seek_Flee.cs
+++ Assets/Scripts/MovementScripts/Seek_Flee.cs
@@ -1,55 +1,153 @@
+using System;
 using UnityEngine;
 
-public class Seek_Flee : MonoBehaviour
+public class Seek_Flee : Agent
 {
     //Temp
-    [SerializeField] private Transform _objetive;
+    [SerializeField] private Agent _objetive;
 
     [SerializeField] private float _maxSpeed = 5;
     [SerializeField] private float _steeringSpeed = 3;
+    [SerializeField] private float _maxDistance = 10;
+    [SerializeField] private float _minDistance = 0.1f;
 
-    [SerializeField] private bool _fleeOrSeek = false; 
-
-    private Vector3 actualVelocity;
+    public enum Movement { Flee,Seek,Arrive,Escape,Pursuit,Evade}
+    [SerializeField] private Movement _movement;
 
     void Update()
     {
-        if (_fleeOrSeek)
+        MovementTipe();
+        transform.position += _actualVelocity * Time.deltaTime;
+        transform.forward = _actualVelocity;
+    }
+
+    private void MovementTipe()
+    {
+        switch (_movement) 
+        { 
+            case Movement.Flee:
+                Flee(_objetive.transform.position);
+                break;
+            case Movement.Seek:
+                Seek(_objetive.transform.position);
+                break;
+            case Movement.Arrive:
+                Arrive(_objetive.transform.position);
+                break;
+            case Movement.Escape:
+                Escape(_objetive.transform.position);
+                break;
+            case Movement.Pursuit:
+                Pursuit(_objetive);
+                break;
+            case Movement.Evade:
+                Evade(_objetive);
+                break;
+        }
+                
+    }
+    //Dado un objetivo, retorna el vector de velocidad deseado que apunta hacia el 
+    private Vector3 CalculateDesired(Vector3 target)
+    {
+        Vector3 desired = (target - transform.position).normalized;
+        return desired * _maxSpeed;
+    }
+
+    //Dado un vector de velocidad deseada, retorna un vector de velocidad que tiene en cuenta la capacidad de giro del agente
+    private Vector3 CalculateSteering(Vector3 desired)
+    {
+        desired *= _maxSpeed;
+        Vector3 steering = desired - _actualVelocity;
+        return Vector3.ClampMagnitude(steering, _steeringSpeed * Time.deltaTime);
+    }
+
+    //Modifica la velocidad del agente para que corresponda con seguir al objetivo
+    public void Seek(Vector3 target)
+    {
+        Vector3 desired = CalculateDesired(target);
+        _actualVelocity += CalculateSteering(desired);
+    }
+
+    public void Flee(Vector3 target)
+    {
+        Vector3 desired = CalculateDesired(target);
+        _actualVelocity -= CalculateSteering(-desired);
+    }
+
+    public void Arrive(Vector3 target)
+    {
+        Vector3 direction = target - transform.position;
+        float distance = direction.magnitude;
+
+        if (distance < _minDistance)
         {
-            Flee();
-        } 
-        else
-        {
-            Seek();
+            _actualVelocity = Vector3.zero;
+            return;
         }
 
-            transform.position += actualVelocity * Time.deltaTime;
-        transform.forward = actualVelocity;
-    }
+        float slowSpeed = _maxSpeed * (distance / _maxDistance);
 
-    public void Seek()
-    {
-        Vector3 desired = (_objetive.position - transform.position).normalized;
+        float desiredSpeed = Mathf.Min(slowSpeed, _maxSpeed);
 
-        desired *= _maxSpeed;
+        Vector3 desired = direction.normalized * desiredSpeed;
 
-        Vector3 Steering = desired - actualVelocity;
+        Vector3 Steering = desired - _actualVelocity;
 
         Steering = Vector3.ClampMagnitude(Steering, _steeringSpeed * Time.deltaTime);
 
-        actualVelocity += Steering;
+        _actualVelocity += Steering;
     }
 
-    public void Flee()
+
+
+    public void Escape(Vector3 target)
     {
-        Vector3 desired = (_objetive.position - transform.position).normalized;
+        Vector3 direction = target - transform.position;
+        float distance = direction.magnitude;
 
-        desired *= _maxSpeed * -1;
+        if (distance > _maxDistance)
+        {
+            _actualVelocity = Vector3.zero;
+            return;
+        }
 
-        Vector3 Steering = desired - actualVelocity;
+        float slowSpeed = _maxSpeed * (_minDistance / distance);
+
+        float desiredSpeed = Mathf.Min(slowSpeed, _maxSpeed);
+
+        Vector3 desired = direction.normalized * desiredSpeed * -1;
+
+        Vector3 Steering = desired - _actualVelocity;
 
         Steering = Vector3.ClampMagnitude(Steering, _steeringSpeed * Time.deltaTime);
 
-        actualVelocity += Steering;
+        _actualVelocity += Steering;
+    }
+
+
+    private Vector3 CalculateFuture(Agent target)
+    {
+        Vector3 direction = target.transform.position - transform.position;
+
+        float distance = direction.magnitude;
+
+        float prediction = distance / (_maxSpeed + target._actualVelocity.magnitude);
+
+        return target.transform.position + target._actualVelocity* prediction;
+
+    }
+    public void Pursuit(Agent target)
+    {
+        Vector3 futurePosition = CalculateFuture(target);
+
+        Seek(futurePosition);
+
+    }
+
+    public void Evade(Agent target)
+    {
+        Vector3 futurePosition = CalculateFuture(target);
+
+        Flee(futurePosition);
     }
 }

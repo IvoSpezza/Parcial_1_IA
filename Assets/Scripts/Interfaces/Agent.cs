5169b4f7610c592b6d46436c110cfc0d4a07cfa5
Arise <83659212+IvoSpezza@users.noreply.github.com>
2026-09-05 17:53:53 -0300

    Funcionamiento de flocking
    Correctamente agregado el funcionamiento de flocking

--- Assets/Scripts/Interfaces/Agent.cs
+++ Assets/Scripts/Interfaces/Agent.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
-public class Agent : MonoBehaviour
+public abstract class Agent : MonoBehaviour
 {
-    public Vector3 _actualVelocity;
+    public Vector3 _actualVelocity { get; protected set; }
 
 }

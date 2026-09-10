8ec07dd7f997d5c97e7c4890e7c9d2133523597c
Arise <83659212+IvoSpezza@users.noreply.github.com>
2026-09-10 13:13:07 -0300

    Evade added
    aprobeche ahora que ya tenia el cazador para añadir tambien el evade del boid.
    Ademas, me traje el separation a el boid mismo, ya que no importa el estado o que, siempre debe evitar colisionar.

--- Assets/ParcialMaterial/Scripts/Boid/MS_BoidControlScript.cs
+++ Assets/ParcialMaterial/Scripts/Boid/MS_BoidControlScript.cs
@@ -1,7 +1,4 @@
-using NUnit.Framework;
-using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class MS_BoidControlScript : MS_Creature
@@ -10,15 +7,17 @@ public class MS_BoidControlScript : MS_Creature
     
     [SerializeField] private FlockingData _dataForFlocking;
 
-    private StateMachine _machine;
+    public StateMachine _machine {  get; private set; }
+
     private List<MS_BoidControlScript> _agents;
+    private bool _isAlive;
 
+    public MS_Hunter _enemy {  get; private set; }
     private SphereCollider _collision;
-
-    public event Action<BoidState> OnChangeState;
 
     private void Awake()
     {
+        _isAlive = true;
         _collision = GetComponent<SphereCollider>();
 
         _collision.radius = _maxSeeDistance;
@@ -29,24 +28,37 @@ public class MS_BoidControlScript : MS_Creature
 
         S_Flocking flocking = new S_Flocking(this, _agents, _dataForFlocking, _maxSpeed, _steering);
 
+        S_Evading evade = new S_Evading(this,_maxSpeed);
+
         _machine.AddState(flocking, BoidState.Flocking);
+        _machine.AddState(evade, BoidState.Evading);
 
         _machine.ChangeState(BoidState.Flocking);
     }
 
     private void Start()
     {
-        
+        RandomMovement();
     }
 
     private void Update()
     {
+        _velocity += CalculateSeparation() * _dataForFlocking._separationWeight;
         _machine.MachineUpdate();
+        
+        transform.position += _velocity * Time.deltaTime;
+        transform.forward = _velocity;
         transform.position = Bounds.instance.OutOfBounds(transform.position);
     }
-
+    
     private void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.TryGetComponent<MS_Hunter>(out MS_Hunter enemy))
+        {
+            _enemy = enemy;
+            _machine.ChangeState(BoidState.Evading);
+        }
+
         if (other.gameObject.TryGetComponent<MS_BoidControlScript>(out MS_BoidControlScript anotherAgent))
         {
           
@@ -57,6 +69,12 @@ public class MS_BoidControlScript : MS_Creature
 
     private void OnTriggerExit(Collider other)
     {
+        if (other.gameObject.TryGetComponent<MS_Hunter>(out MS_Hunter enemy))
+        {
+            _enemy = null;
+            _machine.ChangeState(BoidState.Flocking);
+        }
+
         if (other.gameObject.TryGetComponent<MS_BoidControlScript>(out MS_BoidControlScript anotherAgent))
         {
             _agents.Remove(anotherAgent);
@@ -64,14 +82,34 @@ public class MS_BoidControlScript : MS_Creature
         }
     }
 
-    public void AnimationHasToChange(BoidState newState)
+    private void RandomMovement()
     {
-        OnChangeState?.Invoke(newState);
+        Vector3 randomDirection = new Vector3(Random.Range(-1, 1), 0f, Random.Range(-1, 1));
+        AplyVelocity(randomDirection.normalized * _maxSpeed);
     }
 
-    public void SetVelocity(Vector3 velocity)
+    //Extraje la separacion para que siempre evite colisionar, sin importar el estado
+    private Vector3 CalculateSeparation()
     {
-        _velocity = velocity;
+        Vector3 dessired = default;
+        if (_agents.Count == 0)
+        {
+            return dessired;
+        }
+
+        foreach (MS_BoidControlScript agent in _agents)
+        {
+            if (Vector3.Distance(agent.transform.position, transform.position) <= _dataForFlocking._minEvadeDistance)
+            {
+                dessired += transform.position - agent.transform.position;
+            }
+        }
+        dessired.y = 0;
+        if (dessired.sqrMagnitude < 0.001f)
+        {
+            return Vector3.zero;
+        }
+        return CalculateSteering(dessired.normalized);
     }
 
 }

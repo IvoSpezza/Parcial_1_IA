7a84e1d57846305d8c9b74d008086263b200bb5c
Arise <83659212+IvoSpezza@users.noreply.github.com>
2026-09-11 08:48:15 -0300

    Animator added to state

--- Assets/ParcialMaterial/Scripts/States/State.cs
+++ Assets/ParcialMaterial/Scripts/States/State.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public abstract class CreatureState
 {
     protected float _maxSpeed;
+    protected Animator _animator;
     public virtual void Enter() { }
 
     public virtual void Update() { }

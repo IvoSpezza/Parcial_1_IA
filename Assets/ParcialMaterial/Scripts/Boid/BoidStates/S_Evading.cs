1df09a3d01ffda2ca150578b90f5b3e41f0bb3d6
Arise <83659212+IvoSpezza@users.noreply.github.com>
2026-09-10 21:25:17 -0300

    CAlculate future migration

--- Assets/ParcialMaterial/Scripts/Boid/BoidStates/S_Evading.cs
+++ Assets/ParcialMaterial/Scripts/Boid/BoidStates/S_Evading.cs
@@ -25,23 +25,14 @@ public class S_Evading : CreatureState
     public void Flee(Vector3 target)
     {
         Vector3 desired = (target - _me.transform.position).normalized;
+
         _me.AplyVelocity(_me.CalculateSteering(-desired));
     }
-    private Vector3 CalculateFuture(MS_Creature target)
-    {
-        Vector3 direction = target.transform.position - _me.transform.position;
-
-        float distance = direction.magnitude;
-
-        float prediction = distance / (_maxSpeed + target._velocity.magnitude);
-
-        return target.transform.position + target._velocity * prediction;
-
-    }
+  
     public void Evade(MS_Creature target)
     {
-        Vector3 futurePosition = CalculateFuture(target);
-        futurePosition.y = 0;
+        Vector3 futurePosition = _me.CalculateFuture(target);
+
         Flee(futurePosition);
     }
 }

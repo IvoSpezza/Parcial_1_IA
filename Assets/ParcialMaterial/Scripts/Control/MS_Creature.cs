505c9c14cbbb5a27df10fa49affad7a61ac3908a
Arise <83659212+IvoSpezza@users.noreply.github.com>
2026-09-12 19:19:15 -0300

    arrive added to msCreature

--- Assets/ParcialMaterial/Scripts/Control/MS_Creature.cs
+++ Assets/ParcialMaterial/Scripts/Control/MS_Creature.cs
@@ -44,10 +44,31 @@ public class MS_Creature : MonoBehaviour
 
     }
 
+    public Vector3 Arrive(Vector3 target, float minDistance, float maxDistance)
+    {
+        Vector3 direction = target - transform.position;
+
+        float distance = direction.magnitude;
+
+        if (distance <= minDistance)
+        {
+            return Vector3.zero;
+        }
+
+        float desiredSpeed = _maxSpeed * Mathf.Clamp01(distance / maxDistance);
+
+        Vector3 desired = direction.normalized * desiredSpeed;
+
+        Vector3 Steering = CalculateSteering(desired);
+
+        return Steering;
+    }
+
     public void AplyVelocity(Vector3 velocity)
     {
         velocity.y = 0;
         _velocity += velocity;
+        _velocity = Vector3.ClampMagnitude(_velocity, _maxSpeed);
     }
 
 }

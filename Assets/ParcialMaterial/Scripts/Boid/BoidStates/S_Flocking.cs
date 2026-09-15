7da95994ecd292f87ec01a9fb4eed3f2afdc370a
Arise <83659212+IvoSpezza@users.noreply.github.com>
2026-09-14 21:43:59 -0300

    trap atack added
    Se añadio el comportamiento de buscar puntos de interes, asi como aplicar el correcto uso de la _state machine

--- Assets/ParcialMaterial/Scripts/Boid/BoidStates/S_Flocking.cs
+++ Assets/ParcialMaterial/Scripts/Boid/BoidStates/S_Flocking.cs
@@ -3,32 +3,62 @@ using UnityEngine;
 
 public class S_Flocking : CreatureState
 {
-    private MS_BoidControlScript _me;
-    private List<MS_BoidControlScript> _agents;
+    private MS_BoidControlScript _me;    
     private FlockingData _flockingData;
+    private BoidData _boidData;
+
     private Animator _animation;
-    public S_Flocking(MS_BoidControlScript me, List<MS_BoidControlScript> agents, FlockingData flockingData, float maxSpeed,float steering, Animator animation)
+    public S_Flocking(MS_BoidControlScript me,BoidData boidData, FlockingData flockingData,StateMachine stateMachine, Animator animation)
     {
         _me = me;
-        _agents = agents;
+        _boidData = boidData;
+        _stateMachine = stateMachine;
         _flockingData = flockingData;        
         _animation = animation;            
     }
 
     public override void Enter()
     {
+        _me.CreatureDebugger.SetTitle("Flocking", _me.gameObject.name, Color.green);
         _animation.SetBool("IsSwiming", true);
     }
 
     public override void Update()
-    {
+    {        
         Flocking();
+        _me.transform.position += _me._velocity * Time.deltaTime;
+        _me.transform.forward = _me._velocity;
+
+        if(CheckSurrondings())return;
+        if(CheckTraps())return;
+        _me.CreatureDebugger.selectedDebug("I have " + _boidData._nearAgents.Count + " friends\n " + "Speed: " + _me._velocity, Color.green);
     }
+
 
     public override void Exit()
     {
         _animation.SetBool("IsSwiming", false);
     }    
+
+    private bool CheckSurrondings()
+    {
+        if(_boidData._hunter.TryPeek(out MS_Hunter hunter))
+        {           
+            _stateMachine.ChangeState(BoidState.Evading);
+            return true;
+        }
+        return false;
+    }
+    private bool CheckTraps()
+    {
+        if (_boidData._nearbyTraps.Count > 0)
+        {
+            _stateMachine.ChangeState(BoidState.Atacking);
+            return true;
+        }
+        return false;
+    }
+
 
     private Vector3 CalculateAlignment()
     {
@@ -36,12 +66,12 @@ public class S_Flocking : CreatureState
 
         int deadOnes = 0;
 
-        if (_agents.Count == 0)
+        if (_boidData._nearAgents.Count == 0)
         {
             return Vector3.zero;
         }
 
-        foreach (MS_BoidControlScript agent in _agents)
+        foreach (MS_BoidControlScript agent in _boidData._nearAgents)
         {            
             if (agent._isAlive)
             {
@@ -53,9 +83,9 @@ public class S_Flocking : CreatureState
             }
         }
 
-        if (_agents.Count - deadOnes == 0) return Vector3.zero;
+        if (_boidData._nearAgents.Count - deadOnes == 0) return Vector3.zero;
 
-        dessired /= _agents.Count - deadOnes;
+        dessired /= _boidData._nearAgents.Count - deadOnes;
                 
         return _me.CalculateSteering(dessired.normalized);
     }
@@ -65,9 +95,9 @@ public class S_Flocking : CreatureState
         Vector3 dessired = default;
         int deadOnes = 0;
 
-        if (_agents.Count == 0) return Vector3.zero;
+        if (_boidData._nearAgents.Count == 0) return Vector3.zero;
       
-        foreach (MS_BoidControlScript agent in _agents)
+        foreach (MS_BoidControlScript agent in _boidData._nearAgents)
         {
             if (agent._isAlive)
             {
@@ -79,9 +109,9 @@ public class S_Flocking : CreatureState
             }
         }
 
-        if (_agents.Count - deadOnes == 0) return Vector3.zero;
+        if (_boidData._nearAgents.Count - deadOnes == 0) return Vector3.zero;
 
-        dessired /= (_agents.Count-deadOnes);
+        dessired /= (_boidData._nearAgents.Count -deadOnes);
                
         if (dessired.sqrMagnitude < 0.001f)
         {
@@ -92,7 +122,6 @@ public class S_Flocking : CreatureState
 
     private void Flocking()
     {
-
         Vector3 aligment = CalculateAlignment() * _flockingData._aligmentWeight; 
         Vector3 cohesion = CalculateCohesion() * _flockingData._cohesionWeight;
 

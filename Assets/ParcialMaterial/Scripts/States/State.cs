3a242cc2c75bf57d01f28b6424163559bb6fe57f
Arise <83659212+IvoSpezza@users.noreply.github.com>
2026-09-14 21:38:19 -0300

    State improbed
    Como todos los states usan la state machine, simplemente la agregue de base

--- Assets/ParcialMaterial/Scripts/States/State.cs
+++ Assets/ParcialMaterial/Scripts/States/State.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public abstract class CreatureState
 {
     protected Animator _animator;
+    protected StateMachine _stateMachine;
     public virtual void Enter() { }
 
     public virtual void Update() { }
